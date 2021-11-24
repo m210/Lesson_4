6ccb73d609fb76654b56d91d27a467ad82c175df
[M210®] <m210-2007@mail.ru>
2021-11-24 19:42:25 +0300

    Accomplished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,44 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        task1();
+
+        task2();
+
+        task3();
+    }
+
+    public static void task1() {
+        int i = 1;
+        while(i <= 10) {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+
+        for(i = 10; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void task2() {
+        int friday = 3;
+        for(int i = 1; i <= 31; i++) {
+            if((i % 7) == friday) {
+                System.out.println("Сегодня пятница, " + i + "-e число. Необходимо подготовить отчет");
+            }
+        }
+    }
+
+    public static void task3() {
+        int year = 2021;
+        int prev = year - 200;
+        int next = year + 100;
+
+        for(int i = prev; i <= next; i++) {
+            if((i % 79) == 0)
+                System.out.println(i);
+        }
     }
 }
